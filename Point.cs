95dc02348cf2e2da41333584e4fa6dec11898a4d
Antonina Novak <anovak@gmail.com>
2023-03-03 11:38:05 +0200

    second commit

--- Point.cs
+++ Point.cs
@@ -1,6 +1,14 @@
-﻿namespace Kse.Algorithms.Samples;
-
-public class Point
+﻿namespace Kse.Algorithms.Samples
 {
-    
+    public struct Point
+    {
+        public int Column { get; }
+        public int Row { get; }
+
+        public Point(int column, int row)
+        {
+            Column = column;
+            Row = row;
+        }
+    }
 }
--- Program.cs
+++ Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Kse.Algorithms.Samples;
+
+var generator = new MapGenerator(new MapGeneratorOptions()
+{
+    Height = 35,
+    Width = 90,
+});
+
+string[,] map = generator.Generate();
+new MapPrinter().Print(map);
